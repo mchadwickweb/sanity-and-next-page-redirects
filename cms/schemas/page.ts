4cd9112c41e4a9a45d3e736e7a33d4c140982012
Michael Chadwick <michael@interactstudio.co.uk>
2023-05-04 13:14:43 +0100

    add sanity studio

---
+++ cms/schemas/page.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'page',
+  type: 'document',
+  title: 'Page',
+  fields: [
+    {
+      name: 'title',
+      title: 'Page title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'string',
+    },
+  ],
+}
